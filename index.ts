f60209eeaa3981e92dae8e16c56315ff6fbe8e3b
Emmanuel Flores <eflores@CIBERDEFA.FAA>
2023-08-05 00:32:40 -0300

    Herencia 2

--- index.ts
+++ index.ts
@@ -1,29 +1,61 @@
-class ListaDeCosas {
+import * as fs from "fs"
+class ListaDeCosas 
+{
   name: string;
   cosas: any[] = [];
-  constructor(name: string) {
+
+  constructor(name: string) 
+  {
     // nombre de esta lista
     this.name = name;
   }
-  add(nuevaCosa) {
+
+  add(nuevaCosa) 
+  {
     this.cosas.push(nuevaCosa);
   }
-  getCosas() {
+
+  getCosas() 
+  {
     return this.cosas;
   }
 }
 
-class Product {
+class Product 
+{
   name: string;
   price: number;
   id: number;
-  constructor(name: string, price: number, id: number) {
+  constructor(name: string, price: number, id: number) 
+  {
     this.name = name;
     this.price = price;
     this.id = id;
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas 
+{
+  constructor(name: string)
+  {
+    super(name)
+    let contenidoArchivo = fs.readFileSync(__dirname + "products.json").toString()
+    let productosJson = JSON.parse(contenidoArchivo);
+
+    productosJson.forEach(element => {this.addProduct(element)})    
+  }
+
+  addProduct(producto: Product)
+  {
+    //let productoEncontrado = this.cosas.find(p => p.)
+    this.add(producto);
+  }
+
+  getProduct(id: number):Product
+  {
+    let cosas = this.getCosas();
+    return cosas.find(c=>c.id == id);
+  }
+}
 
 export { ListaDeProductos, Product };
